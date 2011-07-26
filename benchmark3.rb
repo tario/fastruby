8c38c444ff242e97cfa7bcdc2931d8d6bd7e7bcb
Dario <darios3@gmail.com>
2011-07-25 21:46:35 -0300

    implemented benchmark for block with params (its slower than block without params because of the ruby array instanciated for each call to pass the params)

---
+++ benchmark3.rb
@@ -0,0 +1,70 @@
+require "fastruby"
+
+class X
+	fastruby "
+		def foo
+			yield(1,2,3)
+		end
+	"
+end
+
+class Y
+	fastruby "
+		def bar(x)
+			i = 1000000
+			
+			lvar_type(i,Fixnum)
+			
+			ret = 0
+			while i > 0
+				x.foo do |a,b,c|
+				end
+				  i = i - 1
+			end
+			0
+		end
+
+	"
+end
+
+class X2
+	def foo
+		yield
+	end
+end
+
+class Y2
+		def bar(x)
+			i = 1000000
+			
+			ret = 0
+			while i > 0
+				x.foo do
+				  i = i - 1
+				end
+			end
+			0
+		end
+end
+
+
+x = X.new
+y = Y.new
+y2 = Y2.new
+x2 = X2.new
+
+y.bar(x)
+y2.bar(x2)
+
+require 'benchmark'
+
+Benchmark::bm(20) do |b|
+
+	b.report("fastruby") do
+		y.bar(x)
+	end
+
+	b.report("ruby") do
+		y2.bar(x2)
+	end
+end
