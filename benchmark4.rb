0a579df7caa5746c14e4f7735ac15695047e5858
Dario <darios3@gmail.com>
2011-07-27 00:10:11 -0300

    added benchmark for global variable access (write)

---
+++ benchmark4.rb
@@ -0,0 +1,95 @@
+require "fastruby"
+
+class Y
+	fastruby "
+		def bar
+			i = 1000000
+			
+			lvar_type(i,Fixnum)
+			
+			ret = 0
+			while i > 0
+			  $a = 9
+			  i = i - 1
+			end
+			0
+		end
+
+	"
+end
+
+class Y2
+		def bar
+			i = 1000000
+			
+			ret = 0
+			while i > 0
+			  $a = 9
+			  i = i - 1
+			end
+			0
+		end
+end
+
+
+class Y_
+	fastruby "
+		def bar
+			i = 1000000
+			
+			lvar_type(i,Fixnum)
+			
+			ret = 0
+			while i > 0
+			  i = i - 1
+			end
+			0
+		end
+
+	"
+end
+
+class Y2_
+		def bar
+			i = 1000000
+			
+			ret = 0
+			while i > 0
+			  i = i - 1
+			end
+			0
+		end
+end
+
+
+y = Y.new
+y2 = Y2.new
+y_ = Y_.new
+y2_ = Y2_.new
+
+y.bar
+y2.bar
+y_.bar
+y2_.bar
+
+require 'benchmark'
+
+Benchmark::bm(20) do |b|
+
+	b.report("fastruby") do
+		y.bar
+	end
+
+	b.report("ruby") do
+		y2.bar
+	end
+	
+	b.report("lfastruby") do
+		y_.bar
+	end
+
+	b.report("lruby") do
+		y2_.bar
+	end
+	
+end
