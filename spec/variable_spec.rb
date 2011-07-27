eab0258de565dcdef3b0c6e0f286263e2fbb6cb5
Dario <darios3@gmail.com>
2011-07-26 23:44:05 -0300

    +2 failing test: read and write of instance variables

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -27,4 +27,37 @@ describe FastRuby, "fastruby" do
     $au2 = 88
     ::U2.new.foo.should be == 88
   end
+
+  class ::U3
+    attr_accessor :au3
+
+    fastruby "
+      def foo
+        @au3 = 88
+      end
+    "
+  end
+
+  it "should write instance variables" do
+    u3 = ::U3.new
+    u3.foo
+    u3.au3.should be == 88
+  end
+
+  class ::U4
+    attr_accessor :au4
+
+    fastruby "
+      def foo
+        @au4
+      end
+    "
+  end
+
+  it "should read instance variables" do
+    u4 = ::U4.new
+    u4.au4 = 88
+    u4.foo.should be == 88
+  end
+
 end
