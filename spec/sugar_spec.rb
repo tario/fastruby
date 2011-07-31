04912cf16a86bdfbaec55f3f85d544ff1af755b3
Dario <darios3@gmail.com>
2011-07-31 18:26:50 -0300

    +3 failing test: definition of entire classes in one fastruby call

--- spec/sugar_spec.rb
+++ spec/sugar_spec.rb
@@ -18,4 +18,35 @@ describe FastRuby, "fastruby" do
     e1.foo.should be == 12
     e1.bar.should be == 15
   end
+
+  it "should compile fastruby code with class definition and one method" do
+    fastruby "
+      class ::E2
+        def foo
+          12
+        end
+      end
+    "
+
+    e2 = ::E2.new
+    e2.foo.should be == 12
+  end
+
+  it "should compile fastruby code with class definition and two methods" do
+    fastruby "
+      class ::E3
+        def foo
+          12
+        end
+        def bar
+          15
+        end
+      end
+    "
+
+    e3 = ::E3.new
+    e3.foo.should be == 12
+    e3.bar.should be == 15
+  end
+
 end
