6b7295da2beb71d6ea4a7a6300580146d0792a5b
Dario <darios3@gmail.com>
2011-08-05 20:26:38 -0300

    +1 passing test: calling singleton method from outside fastruby

--- spec/singleton_spec.rb
+++ spec/singleton_spec.rb
@@ -24,4 +24,26 @@ describe FastRuby, "fastruby" do
     R2.bar{67}.should be == 67
   end
 
+  it "should call singleton methods from outside" do
+    class R3
+      def bar
+        12
+      end
+    end
+
+    fastruby '
+      class R4
+        def foo(x)
+          def x.bar
+            24
+          end
+        end
+      end
+    '
+
+    r3 = R3.new
+    R4.new.foo(r3)
+    r3.bar.should be == 24
+  end
+
 end
