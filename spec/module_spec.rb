57b51249265d7dafe70b24c3244516268aac5800
Dario <darios3@gmail.com>
2011-08-06 12:27:09 -0300

    +1 failing test: defining of module with other code

--- spec/module_spec.rb
+++ spec/module_spec.rb
@@ -7,4 +7,14 @@ describe FastRuby, "fastruby" do
       end
     "
   end
+
+  it "should alow definition of modules with other code" do
+    fastruby '
+      print "defining module\n"
+
+      module H2
+      end
+    '
+  end
+
 end
