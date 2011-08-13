ee240d1bcb84a48e19b46250f1b3c53318a318b0
Dario <darios3@gmail.com>
2011-08-13 12:17:31 -0300

    +2 failing tests: break

---
+++ spec/block/break_spec.rb
@@ -0,0 +1,57 @@
+require "fastruby"
+
+describe FastRuby, "fastruby" do
+  class ::V1
+    fastruby "
+      def foo(ary)
+        sum = 0
+        ary.each do |a|
+          sum = sum + a
+        end
+
+        sum
+      end
+    "
+  end
+
+  it "should execute basic test iterating an array" do
+    ::V1.new.foo([1,2,3]).should be == 6
+  end
+
+  class ::V2
+    fastruby "
+      def foo(ary)
+        sum = 0
+        ary.each do |a|
+          sum = sum + a
+          break
+        end
+
+        sum
+      end
+    "
+  end
+
+  it "should execute basic test iterating an array with a break" do
+    ::V2.new.foo([1,2,3]).should be == 1
+  end
+
+  class ::V3
+    fastruby "
+      def foo(ary)
+        sum = 0
+        ary.each do |a|
+          sum = sum + a
+          break if a == 2
+        end
+
+        sum
+      end
+    "
+  end
+
+  it "should execute basic test iterating an array with a conditional break" do
+    ::V3.new.foo([1,2,3]).should be == 3
+  end
+
+end
