2cbf627c43d8d9a8252bfd5a64b4c182295abeb5
Dario <darios3@gmail.com>
2011-09-05 23:30:12 -0300

    +1 failing test: lambda call with another calls overwriting the deprecated stack before call the lambda

--- spec/block/lambda_spec.rb
+++ spec/block/lambda_spec.rb
@@ -15,4 +15,25 @@ describe FastRuby, "fastruby" do
   it "lambda must be able to access local variables" do
     ::LL1.new.foo.call(16).should be == 32
   end
+
+    fastruby "
+  class ::LL2
+      def foo
+        a = 16
+        lambda {|x|
+          a+x
+        }
+      end
+
+      def bar
+      end
+  end
+    "
+
+  it "lambda must be able to access local variables, after another unrelated method is called" do
+    ll2 = ::LL2.new
+    lambda_object = ll2.foo
+    ::LL2.new.bar
+    lambda_object.call(16).should be == 32
+  end
 end
