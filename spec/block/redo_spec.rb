424863965221d9502895868346deb4f6b0ea4a71
Dario <darios3@gmail.com>
2011-10-01 00:00:14 -0300

    +1 failing test: illegal redo

--- spec/block/redo_spec.rb
+++ spec/block/redo_spec.rb
@@ -48,4 +48,20 @@ describe FastRuby, "fastruby redo statement" do
     wb2.foo.should be == 10
   end
 
+  class ::WB3
+    fastruby "
+      def foo
+        redo
+      end
+    "
+  end
+
+  it "should raise LocalJumpError when invoked illegal redo" do
+    wb3 = ::WB3.new
+
+    lambda {
+      wb3.foo
+    }.should raise_error(LocalJumpError)
+  end
+
 end
