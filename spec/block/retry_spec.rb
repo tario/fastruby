15d3c09623460139c9591f001ea6a73e75f5b87c
Dario <darios3@gmail.com>
2011-10-01 12:41:41 -0300

    +1 failing test: retry on methods calls with number of arguments greater than zero

--- spec/block/retry_spec.rb
+++ spec/block/retry_spec.rb
@@ -39,4 +39,43 @@ describe FastRuby, "fastruby retry statement" do
     wg1.b.should be == 10
   end
 
+
+  class ::WG2
+    attr_reader :a, :b
+
+    def initialize
+      @a = 0
+      @b = 0
+    end
+
+    fastruby "
+      def bar(x)
+        @a = @a + 1
+
+        yield(1)
+        yield(2)
+        yield(3)
+      ensure
+        @b = @b + 1
+      end
+
+      def foo
+        sum = 0
+        bar(0) do |x|
+          sum = sum + 1
+          retry if x == 3 and sum < 30
+        end
+
+        sum
+      end
+    "
+  end
+
+  it "should work with a method more arguments than zero" do
+    wg2 = ::WG2.new
+    wg2.foo.should be == 30
+    wg2.a.should be == 10
+    wg2.b.should be == 10
+  end
+
 end
