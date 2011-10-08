18108f83a5b5d2ab4b2b8ed89a2caa9074166e4a
Dario <darios3@gmail.com>
2011-10-08 12:51:13 -0300

    +1 failing test: normal arguments plus array arguments

--- spec/defn/multiple_args_spec.rb
+++ spec/defn/multiple_args_spec.rb
@@ -54,5 +54,59 @@ describe FastRuby, "fastruby" do
     }.should_not raise_error
   end  
   
+  it "should allow define method with normal argument plus array arguments and call with one argument" do
+    lambda {
+      fastruby "
+        class ::CF5
+          def foo(a, *x)
+            x
+          end
+        end
+      "
+      
+      ::CF5.new.foo(1).should be == []
+    }.should_not raise_error
+  end  
+
+  it "should allow define method with normal argument plus array arguments and call with two arguments" do
+    lambda {
+      fastruby "
+        class ::CF6
+          def foo(a, *x)
+            x
+          end
+        end
+      "
+      
+      ::CF6.new.foo(1,2).should be == [2]
+    }.should_not raise_error
+  end  
   
+  it "should allow define method with normal argument plus array arguments and call with one argument" do
+    lambda {
+      fastruby "
+        class ::CF7
+          def foo(a, *x)
+            a
+          end
+        end
+      "
+      
+      ::CF7.new.foo(1).should be == 1
+    }.should_not raise_error
+  end  
+
+  it "should allow define method with normal argument plus array arguments and call with two arguments" do
+    lambda {
+      fastruby "
+        class ::CF8
+          def foo(a, *x)
+            a
+          end
+        end
+      "
+      
+      ::CF8.new.foo(1,2).should be == 1
+    }.should_not raise_error
+  end  
 end
