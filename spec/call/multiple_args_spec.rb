c881665a343f222f4a5650515bd742cb05b60227
Dario <darios3@gmail.com>
2011-10-09 19:40:07 -0300

    +2 failing test: call with splat argument expansion and blocks

--- spec/call/multiple_args_spec.rb
+++ spec/call/multiple_args_spec.rb
@@ -29,5 +29,37 @@ describe FastRuby, "fastruby" do
         end
         
         ::CY2.new.foo([1,2,3]).should be == [100,1,2,3]
+end
+
+  it "should allow basic array expansion with block" do
+        class ::CY3
+          def bar(*x)
+            x
+          end
+        fastruby "
+          def foo(x)
+            bar(*x) do
+	        end
+          end
+        "
+        end
+        
+        ::CY3.new.foo([1,2,3]).should be == [1,2,3]
+  end
+
+  it "should allow basic array expansion plus single argument with block" do
+        class ::CY4
+          def bar(*x)
+            x
+          end
+        fastruby "
+          def foo(x)
+            bar(100,*x) do
+		   end
+          end
+        "
+        end
+        
+        ::CY4.new.foo([1,2,3]).should be == [100,1,2,3]
   end
 end
