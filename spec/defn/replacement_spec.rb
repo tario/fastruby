4ced6f0ced6f0326404fbf4bf7527888d1a6c31e
Dario <darios3@gmail.com>
2011-10-12 18:10:35 -0300

    +1 failing test: replacement of methods called from fastruby

--- spec/defn/replacement_spec.rb
+++ spec/defn/replacement_spec.rb
@@ -23,4 +23,29 @@ describe FastRuby, "fastruby" do
     ::JU1.new.foo.should be == 9
   end
 
+  it "should allow replace methods after they are called and compiled at runtime (through other method)" do
+    fastruby "
+      class JU2
+        def foo
+          6
+        end
+        
+        def bar
+          foo
+        end
+      end
+    "
+    
+    ::JU2.new.bar
+    
+    fastruby "
+      class JU2
+        def foo
+          9
+        end
+      end
+    "
+    
+    ::JU2.new.bar.should be == 9
+  end
 end
