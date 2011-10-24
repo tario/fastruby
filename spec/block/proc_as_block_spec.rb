a08543db0ccd0bf19ed4b410067af95dfebe2eec
Dario <darios3@gmail.com>
2011-10-24 11:52:31 -0300

    +1 failing test: proc as block receiving arguments

--- spec/block/proc_as_block_spec.rb
+++ spec/block/proc_as_block_spec.rb
@@ -45,4 +45,32 @@ describe FastRuby, "fastruby" do
     executed.should be == 1
   end
 
+  class ::VY3
+    fastruby "
+      def foo
+        yield(1)
+      end
+      
+      def bar(block)
+        foo(&block)
+      end
+    "
+  end
+
+  it "should allow block as proc when calling from fastruby with arguments" do
+    executed = 0
+    recv_x = nil
+    
+    block = proc do |x|
+      recv_x = x       
+      executed = 1
+    end
+    
+    ::VY3.new.bar(block)
+    
+    recv_x.should be == 1
+    executed.should be == 1
+  end
+
+
 end
