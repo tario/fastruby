b179fa8c09f601adeea658fc44cd598fcac38bea
Dario <darios3@gmail.com>
2011-11-01 14:50:43 -0300

    fastruby v0.0.15

--- lib/fastruby.rb
+++ lib/fastruby.rb
@@ -25,5 +25,5 @@ require "fastruby/custom_require"
 require "fastruby/set_tree"
 
 module FastRuby
-  VERSION = "0.0.14" unless defined? FastRuby::VERSION
+  VERSION = "0.0.15" unless defined? FastRuby::VERSION
 end
