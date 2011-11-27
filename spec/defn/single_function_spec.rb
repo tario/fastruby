c59698eee35685e910c885cfb3537570a4bc8f31
Dario <darios3@gmail.com>
2011-11-27 13:11:57 -0300

    +2 failing tests: defined method from inside another method

--- spec/defn/single_function_spec.rb
+++ spec/defn/single_function_spec.rb
@@ -16,4 +16,35 @@ describe FastRuby, "fastruby" do
     "
     foo
   end
+  
+  it "should allow define method from inside another method" do
+    class ::YZU1
+    fastruby "
+      def foo
+        def bar
+          77
+        end
+      end
+    "
+    end
+    
+    ::YZU1.new.foo
+  end
+  
+  it "should allow define and call method from inside another method" do
+    class ::YZU2
+    fastruby "
+      def foo
+        def bar
+          99
+        end
+      end
+    "
+    end
+
+    yzu2 = ::YZU2.new
+    yzu2.foo
+    yzu2.bar.should be == 99
+  end  
+
 end
