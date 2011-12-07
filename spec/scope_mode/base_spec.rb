82ae8f5cc635cbdaf6fa87314f8e25c471382f72
Dario <darios3@gmail.com>
2011-12-07 20:23:43 -0300

    +1 failing test: read after call in a block

--- spec/scope_mode/base_spec.rb
+++ spec/scope_mode/base_spec.rb
@@ -82,6 +82,16 @@ describe FastRuby::ScopeModeHelper, "scope mode helper" do
     ).should be == :linear
   end
 
+  it "read AFTER call should return :dag scope" do
+    FastRuby::ScopeModeHelper.get_scope_mode(
+      $parser.parse "def foo(a,b) 
+        a+b
+        c=55
+        c
+      end"
+    ).should be == :dag
+  end
+
   it "method call AFTER read inside while should return :dag scope" do
     FastRuby::ScopeModeHelper.get_scope_mode(
       $parser.parse "def foo(a,b)
