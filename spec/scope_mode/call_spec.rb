1b59ba7dc07b4d3f2f9d194091023e5bd7b85353
Dario <darios3@gmail.com>
2011-12-10 11:23:42 -0300

    +1 passing test: two nested calls should return :dag

---
+++ spec/scope_mode/call_spec.rb
@@ -0,0 +1,16 @@
+require "fastruby"
+require "sexp"
+require "ruby_parser"
+require "fastruby/translator/scope_mode_helper"
+
+$parser = RubyParser.new
+
+describe FastRuby::ScopeModeHelper, "scope mode helper" do
+  it "method with two nested calls refering local vars should return :dag scope mode" do
+    FastRuby::ScopeModeHelper.get_scope_mode(
+      $parser.parse "def foo(a,b,c) 
+        a+b+c
+      end"
+    ).should be == :dag
+  end
+end
