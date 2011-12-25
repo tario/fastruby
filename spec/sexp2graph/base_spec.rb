9cda2a20736bf1e8d29556ba4af858d8bab6c391
Dario Seminara <darios3@gmail.com>
2011-12-25 12:49:37 -0300

    refactored sexp2graph test to avoid duplicated code

--- spec/sexp2graph/base_spec.rb
+++ spec/sexp2graph/base_spec.rb
@@ -4,20 +4,26 @@ require "sexp"
 require "ruby_parser"
 
 describe FastRuby::FastRubySexp, "FastRubySexp" do
-  it "should have edges" do
-    FastRuby::FastRubySexp.parse("def foo; end").should respond_to(:edges)
-  end
-
-  it "should have two edges for empty method" do
-    sexp = FastRuby::FastRubySexp.parse("def foo; end")
+  def get_edges(code)
+    sexp = FastRuby::FastRubySexp.parse(code)
 
     edges = Array.new
     sexp[3].edges.each do |tree_orig, tree_dest|
       edges << [tree_orig, tree_dest]
     end
 
-    edges.should include([sexp[3][1],sexp[3]]) # scope after block
-    edges.should include([sexp[3][1][1],sexp[3][1]]) # block after nil
+    yield(sexp, edges)
+  end  
+
+  it "should have edges" do
+    FastRuby::FastRubySexp.parse("def foo; end").should respond_to(:edges)
+  end
+
+  it "should have two edges for empty method" do
+    get_edges("def foo; end") do |sexp, edges|
+      edges.should include([sexp[3][1],sexp[3]]) # scope after block
+      edges.should include([sexp[3][1][1],sexp[3][1]]) # block after nil
+    end
   end
 
 end
