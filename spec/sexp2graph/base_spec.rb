44cb94f8a51cdec05aa97284bfe9c9fed073d6aa
Dario Seminara <darios3@gmail.com>
2011-12-25 12:30:49 -0300

    +1 failing test: edges on empty method

--- spec/sexp2graph/base_spec.rb
+++ spec/sexp2graph/base_spec.rb
@@ -7,4 +7,17 @@ describe FastRuby::FastRubySexp, "FastRubySexp" do
   it "should have edges" do
     FastRuby::FastRubySexp.parse("def foo; end").should respond_to(:edges)
   end
+
+  it "should have one edge for empty method" do
+    sexp = FastRuby::FastRubySexp.parse("def foo; end")
+
+    edges = Array.new
+    sexp[3].edges.each do |tree_orig, tree_dest|
+      edges << [tree_orig, tree_dest]
+    end
+
+    edges.should include([sexp[3][1],sexp[3]]) # scope after block
+    edges.should include([sexp[3][1][1],sexp[3][1]]) # block after nil
+  end
+
 end
