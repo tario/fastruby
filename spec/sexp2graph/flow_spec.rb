d9c99cc239722518344dd6e6744ebfcd91f4af86
Dario Seminara <darios3@gmail.com>
2011-12-25 14:29:42 -0300

    +1 failing test: if edges

---
+++ spec/sexp2graph/flow_spec.rb
@@ -0,0 +1,36 @@
+require "fastruby"
+require "fastruby/sexp_extension"
+require "sexp"
+require "ruby_parser"
+
+describe FastRuby::FastRubySexp, "FastRubySexp" do
+  def get_defn_edges(code)
+    sexp = FastRuby::FastRubySexp.parse(code)
+
+    edges = Array.new
+    sexp[3].edges.each do |tree_orig, tree_dest|
+      edges << [tree_orig, tree_dest]
+    end
+
+    yield(sexp, edges)
+  end  
+
+  def get_edges(code)
+    sexp = FastRuby::FastRubySexp.parse(code)
+
+    edges = Array.new
+    sexp.edges.each do |tree_orig, tree_dest|
+      edges << [tree_orig, tree_dest]
+    end
+
+    yield(sexp, edges)
+  end
+
+  it "should have two edges for if" do
+    get_edges("if a; b; else; c; end") do |sexp, edges|
+      edges.size.should be == 2
+      edges.should include([sexp[1],sexp[2]]) # true block after condition
+      edges.should include([sexp[1],sexp[3]]) # false block after condition
+    end
+  end
+end
