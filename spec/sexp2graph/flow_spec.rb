965752d0bc1cc72acd5b220f88a5e30e2eff2264
Dario Seminara <darios3@gmail.com>
2011-12-28 20:13:39 -0300

    refactored test of case statement to use new syntax

--- spec/sexp2graph/flow_spec.rb
+++ spec/sexp2graph/flow_spec.rb
@@ -94,24 +94,14 @@ describe FastRuby::FastRubySexp, "FastRubySexp" do
       end
       ") do |sexp, edges|
 
-      variable_node = sexp[1]
-      when_node_1 = sexp[2]
-      when_node_1_condition = when_node_1[1][1]
-      when_node_1_body = when_node_1[2]
-      when_node_2 = sexp[3]
-      when_node_2_condition = when_node_2[1][1]
-      when_node_2_body = when_node_2[2]
-      else_body = sexp.last
-
-      edges.should include([variable_node,when_node_1_condition])
-      edges.should include([when_node_1_condition,when_node_1_body])
-      edges.should include([when_node_1_condition,when_node_2_condition])
-      edges.should include([when_node_1_body,sexp])
-      edges.should include([when_node_2_condition,when_node_2_body])
-      edges.should include([when_node_2_condition,else_body])
-      edges.should include([when_node_2_body,sexp])
-      edges.should include([else_body,sexp])
-      
+      edges.tr_edges :a => :b,
+                     :b => :c, :b => :d,
+                     :c => sexp, 
+                     :d => :e, :d => :f,
+                     :e => sexp,
+                     :f => sexp do |a,b|
+        edges.should include([a,b])
+      end       
     end
   end
 
