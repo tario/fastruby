23a8ccfdb44a494397015b23808a2823e9c45f21
Dario Seminara <darios3@gmail.com>
2011-12-28 21:19:32 -0300

    refactored all flow specs to use the new syntax

--- spec/sexp2graph/flow_spec.rb
+++ spec/sexp2graph/flow_spec.rb
@@ -7,59 +7,30 @@ require "edges_helper"
 describe FastRuby::FastRubySexp, "FastRubySexp" do
   include EdgesHelper
 
-  it "should have two edges for if" do
-    get_edges("if a; b; else; c; end") do |sexp, edges|
-      edges.size.should be == 4
-      edges.should include([sexp[1],sexp[2]]) # true block after condition
-      edges.should include([sexp[1],sexp[3]]) # false block after condition
-      edges.should include([sexp[2],sexp]) # if after true block
-      edges.should include([sexp[3],sexp]) # if after false block
-    end
+  assert_graph("should have two edges for if","if a; b; else; c; end",4) do |sexp, edges|
+    {:a => [:b,:c], :b => sexp, :c => sexp}
   end
 
-  it "should have only one edge for if without else" do
-    get_edges("if a; b; end") do |sexp, edges|
-      edges.size.should be == 3
-      edges.should include([sexp[1],sexp[2]]) # true block after condition
-      edges.should include([sexp[2],sexp]) # if after true block
-      edges.should include([sexp[1],sexp]) # if after condition
-    end
+  assert_graph("should have only one edge for if without else","if a; b; end",3) do |sexp, edges|
+    {:a => [:b,sexp], :b => sexp}      
   end
 
-  it "should have only one edge for unless" do
-    get_edges("unless a; b; end") do |sexp, edges|
-      edges.size.should be == 3
-
-      edges.should include([sexp[1],sexp[3]]) # false block after condition
-      edges.should include([sexp[3],sexp]) # if after false block
-      edges.should include([sexp[1],sexp]) # if after condition
-    end
+  assert_graph("should have only one edge for unless","unless a; b; end",3) do |sexp, edges|
+   {:a => [:b,sexp], :b => sexp}
   end
 
-  it "should connect with the first node" do
-    get_edges("if a; b.foo(x); end") do |sexp, edges|
-      call_tree = sexp[2]
-      edges.should include([sexp[1],call_tree[1]]) # call recv after condition
-    end
+  assert_graph("should connect with the first node","if a; b.foo(x); end") do |sexp, edges|
+    if_body = sexp.find_tree(:if)[2]
+    {:a => :b, :b => :x, :x => if_body, if_body => sexp }
   end
 
-  it "should connect previous call on block with condition of next if" do
-    get_edges("foo; if a; b; end") do |sexp, edges|
-      edges.should include([sexp[1],sexp[2][1]]) # if condition after call
-    end
+  assert_graph("should connect previous call on block with condition of next if","foo; if a; b; end") do |sexp, edges|
+    {:foo => :a }
   end
 
-  it "should create circular connection from while nodes" do
-    get_edges("while(a); foo; bar; end") do |sexp, edges|
-      condition_tree = sexp[1]
-      execution_tree = sexp[2]
-
-      edges.size.should be == 5
-
-      edges.should include([condition_tree,execution_tree[1]])
-      edges.should include([execution_tree,condition_tree])
-      edges.should include([condition_tree,sexp])
-    end
+  assert_graph("should create circular connection from while nodes",
+              "while(a); foo; bar; end",5) do |sexp, edges|
+    { :a => :foo, sexp.find_tree(:block) => :a, :a => sexp}
   end
 
   assert_graph("should connect break inside while nodes with while", "while(a); foo; break; bar; end", 7) do |sexp,edges|
@@ -72,8 +43,7 @@ describe FastRuby::FastRubySexp, "FastRubySexp" do
     
   end
 
-  it "should connect previous call on block with condition of next while" do
-    get_edges("
+  assert_graph("should connect previous call on block with condition of next while","
       case a
       when b
         c
@@ -84,19 +54,14 @@ describe FastRuby::FastRubySexp, "FastRubySexp" do
       end
       ") do |sexp, edges|
 
-      edges.tr_edges :a => :b,
-                     :b => :c, :b => :d,
+     { :a => :b,:b => [:c,:d],
                      :c => sexp, 
-                     :d => :e, :d => :f,
+                     :d => [:e, :f],
                      :e => sexp,
-                     :f => sexp do |a,b|
-        edges.should include([a,b])
-      end       
-    end
+                     :f => sexp }
   end
 
-  it "should connect nodes of multiple options of when" do
-    get_edges("
+  assert_graph("should connect nodes of multiple options of when","
       case a
       when b,c
         d
@@ -105,13 +70,10 @@ describe FastRuby::FastRubySexp, "FastRubySexp" do
       end
       ") do |sexp, edges|
 
-      edges.tr_edges :a => :b, 
-                              :b => :c, :b => :d,
-                              :c => :d, :c => :e,
-                              :d => sexp,
-                              :e => sexp do |a,b|
-        self.should include([a,b])
-      end
-    end
+    {:a => :b, 
+     :b => [:c, :d],
+     :c => [:d => :e],
+     :d => sexp,
+     :e => sexp }
   end
 end
