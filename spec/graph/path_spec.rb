0feb07019cfaf4fbf6a199150390648a9ac7a500
Dario Seminara <darios3@gmail.com>
2012-01-15 11:03:52 -0300

    +2 passing test: multiple paths of length three

--- spec/graph/path_spec.rb
+++ spec/graph/path_spec.rb
@@ -33,5 +33,7 @@ describe FastRuby::Graph, "fastruby sexp graph" do
   assert_graph_paths(1, [[1,2],[1,3]], 1 => [2,3] )
 
   assert_graph_paths(1, [[1,2,3]], 1 => [2], 2 => [3] )
+  assert_graph_paths(1, [[1,2,3],[1,2,4]], 1 => [2], 2 => [3,4] )
+  assert_graph_paths(1, [[1,2,4],[1,3,4]], 1 => [2,3], 2 => [4],3 => [4] )
 end
 
