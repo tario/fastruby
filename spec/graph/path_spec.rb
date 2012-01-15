2ebc600212a2c0b3c3c68510a29c115f72363f59
Dario Seminara <darios3@gmail.com>
2012-01-15 10:51:42 -0300

    +1 failng test: path of three vertexes

--- spec/graph/path_spec.rb
+++ spec/graph/path_spec.rb
@@ -31,5 +31,7 @@ describe FastRuby::Graph, "fastruby sexp graph" do
   assert_graph_paths(4, [[4,2]], 4 => [2] )
 
   assert_graph_paths(1, [[1,2],[1,3]], 1 => [2,3] )
+
+  assert_graph_paths(1, [[1,2,3]], 1 => [2], 2 => [3] )
 end
 
