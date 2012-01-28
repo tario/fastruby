e959c2b7d2e2f8bf5c03f75c4dfcd8b1764e4571
Dario Seminara <darios3@gmail.com>
2012-01-28 11:44:31 -0300

    refactored specs to use fs instead of s and conversion

--- spec/reductor/base_spec.rb
+++ spec/reductor/base_spec.rb
@@ -16,8 +16,8 @@ describe FastRuby, "fastruby" do
   it "should reduce for statements into each calls" do
    reductor = FastRuby::Reductor.new
    
-   original_tree = FastRuby::FastRubySexp.from_sexp s(:for, s(:call, nil, :b, s(:arglist)), s(:lasgn, :a), s(:call, nil, :c, s(:arglist)))
-   expected_tree = FastRuby::FastRubySexp.from_sexp s(:iter, s(:call, s(:call, nil, :b, s(:arglist)), :each, s(:arglist)), s(:lasgn, :a), s(:call, nil, :c, s(:arglist))).to_fastruby_sexp
+   original_tree = fs(:for, fs(:call, nil, :b, fs(:arglist)), fs(:lasgn, :a), fs(:call, nil, :c, fs(:arglist)))
+   expected_tree = fs(:iter, fs(:call, fs(:call, nil, :b, fs(:arglist)), :each, fs(:arglist)), fs(:lasgn, :a), fs(:call, nil, :c, fs(:arglist)))
    obtained_tree = reductor.reduce original_tree
    
    obtained_tree.should be == expected_tree
@@ -29,9 +29,9 @@ describe FastRuby, "fastruby" do
   it "should reduce call statements into if arrays" do
    reductor = FastRuby::Reductor.new
    
-   original_tree = FastRuby::FastRubySexp.from_sexp s(:case, 
-      s(:call, nil, :a, s(:arglist)), 
-      s(:when, s(:array, s(:call, nil, :b, s(:arglist))), s(:call, nil, :c, s(:arglist))), 
+   original_tree = fs(:case, 
+      fs(:call, nil, :a, fs(:arglist)), 
+      fs(:when, fs(:array, fs(:call, nil, :b, fs(:arglist))), fs(:call, nil, :c, fs(:arglist))), 
       nil)
 
    obtained_tree = reductor.reduce original_tree
