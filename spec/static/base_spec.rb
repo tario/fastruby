f7142d8305586deadba69daf1a62e7b79845cccd
Dario Seminara <darios3@gmail.com>
2012-01-28 23:08:33 -0300

    +1 failing test: should accept _native to accept native C semantic

--- spec/static/base_spec.rb
+++ spec/static/base_spec.rb
@@ -15,4 +15,18 @@ describe FastRuby::FastRubySexp, "FastRubySexp" do
     
     STATICX1.new.foo(100).should be == 100
   end
+  
+  it "should accept _native to accept native C semantic" do
+    fastruby "
+    class STATICX2
+      def foo
+        _static {
+           INT2FIX(_native{100})
+        }
+      end
+    end
+    "
+    
+    STATICX2.new.foo.should be == 100
+  end
 end
