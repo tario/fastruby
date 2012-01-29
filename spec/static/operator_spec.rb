69aeb12c8d53e7f7a455de9c2d3dd91d26bf5d94
Dario Seminara <darios3@gmail.com>
2012-01-28 23:40:39 -0300

    +3 failing tests: should accept native operator * with two numbers

--- spec/static/operator_spec.rb
+++ spec/static/operator_spec.rb
@@ -15,4 +15,46 @@ describe FastRuby::FastRubySexp, "FastRubySexp" do
     
     STATICX1_1.new.foo(1).should be == 2
   end
+  
+  it "should accept native operator - with two numbers" do
+    fastruby "
+    class STATICX1_1
+      def foo(a)
+        _static {
+           INT2FIX(FIX2INT(a)-_native{8})
+        }
+      end
+    end
+    "
+    
+    STATICX1_1.new.foo(10).should be == 2
+  end
+  
+  it "should accept native operator * with two numbers" do
+    fastruby "
+    class STATICX1_1
+      def foo(a)
+        _static {
+           INT2FIX(FIX2INT(a)*_native{3})
+        }
+      end
+    end
+    "
+    
+    STATICX1_1.new.foo(3).should be == 9
+  end
+  
+  it "should accept native operator / with two numbers" do
+    fastruby "
+    class STATICX1_1
+      def foo(a)
+        _static {
+           INT2FIX(FIX2INT(a)/_native{3})
+        }
+      end
+    end
+    "
+    
+    STATICX1_1.new.foo(9).should be == 3
+  end
 end
