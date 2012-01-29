3fefa4b903e60393cabab18f1d5e269cfb786ec0
Dario Seminara <darios3@gmail.com>
2012-01-29 00:15:32 -0300

    +12 test pass: operator <= and >=

--- lib/fastruby/translator/modules/static.rb
+++ lib/fastruby/translator/modules/static.rb
@@ -52,7 +52,7 @@ module FastRuby
         recv_tree = tree[1]
         
         if recv_tree
-          if (not tree[2].to_s =~ /^[a-zA-Z]/) and tree[2].to_s.size == 1
+          if (not tree[2].to_s =~ /^[a-zA-Z]/) and tree[2].to_s.size <= 2
             code = "( ( #{to_c(tree[1])} )#{tree[2]}(#{to_c(tree[3][1])}) )"
           else
             raise "invalid static call #{method_name}"
