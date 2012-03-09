21e6d4e0500c9c65377289632b7757cca2b0a3da
Dario Seminara <darios3@gmail.com>
2012-03-09 20:51:56 -0300

    +1 failing test: should allow define fastruby method using fastruby block with fastruby_only

--- spec/sugar/base_spec.rb
+++ spec/sugar/base_spec.rb
@@ -13,4 +13,17 @@ describe FastRuby, "fastruby sexp graph" do
     FRBSUGAR1.new.frbsugar1_foo(100).should be == 101
   end
 
+  it "should allow define fastruby method using fastruby block with fastruby_only" do
+    class FRBSUGAR2
+      fastruby(:fastruby_only => true) do
+        def frbsugar2_foo(a)
+          _static{INT2FIX(FIX2INT(a)+1)}
+        end
+      end
+    end
+
+    lambda {
+      FRBSUGAR2.new.frbsugar2_foo(100)
+    }.should raise_error(NoMethodError)
+  end
 end
