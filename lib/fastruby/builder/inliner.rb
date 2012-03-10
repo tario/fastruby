79c240aea964c551de315c523a9e8fb744b2ed1d
Dario Seminara <darios3@gmail.com>
2012-03-10 18:43:40 -0300

    refactor: removed unnecessary requuire of set and sexp from inliner.rb

--- lib/fastruby/builder/inliner.rb
+++ lib/fastruby/builder/inliner.rb
@@ -18,8 +18,6 @@ you should have received a copy of the gnu general public license
 along with fastruby.  if not, see <http://www.gnu.org/licenses/>.
 
 =end
-require "set"
-require "sexp"
 require "define_method_handler"
 require "fastruby/modules"
 
