4dcbadcf775707aeb8a80117916a7453bc561c78
Dario Seminara <darios3@gmail.com>
2012-03-14 22:37:14 -0300

    implemented 16 tests to add cover to current fixnum implementations (<, >, + and -), failing tests

---
+++ spec/stdlib/fixnum_spec.rb
@@ -0,0 +1,75 @@
+require "fastruby"
+
+describe FastRuby, "fastruby fixnum stdlib" do
+  
+  $fixnum_class_num = 0
+  def self.test_op_with_type(recv, op, parameter, expected = recv.send(op, parameter))
+    _classname = "FXNUMTEST#{$fixnum_class_num}"
+    
+    it "receiver #{recv}.#{op}(#{parameter}) should return #{expected} #{expected.class}" do
+    fastruby(
+      "class #{_classname}
+          def foo(parameter)
+            #{recv}.#{op}(parameter)
+          end
+        end
+      "
+    
+    )
+    
+      eval(_classname).new.foo(parameter).should be == expected
+    end
+    
+    $fixnum_class_num = $fixnum_class_num + 1
+  end
+
+  def self.test_op(recv, op, parameter, expected = recv.send(op, parameter))
+    _classname = "FXNUMTEST#{$fixnum_class_num}"
+    
+    it "receiver #{recv}.#{op}(#{parameter}) should return #{expected} #{expected.class}" do
+    fastruby(
+      "class #{_classname}
+          def foo(parameter)
+            #{recv}.#{op}(parameter.call)
+          end
+        end
+      "
+    
+    )
+    
+      eval(_classname).new.foo( lambda{parameter} ).should be == expected
+    end
+    
+    $fixnum_class_num = $fixnum_class_num + 1
+  end
+
+  
+  class CustomValue
+    def +(x)
+      42
+    end
+
+    def -(x)
+      32
+    end
+    
+    def <(x)
+      true
+    end
+
+    def >(x)
+      false
+    end
+    
+    def coerce(other)
+      return self, other
+    end
+  end
+
+  [:+, :-, :<, :>].each do |op|
+    [1,1.1,10**10,CustomValue.new].each do |value|
+      test_op_with_type(1,op,value)
+      test_op(1,op,value)
+    end
+  end
+end
