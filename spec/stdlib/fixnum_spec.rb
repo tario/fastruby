540d313379986bd8754aa8e36ea6b0592f4547bd
Dario Seminara <darios3@gmail.com>
2012-03-14 23:32:29 -0300

    added passing tests for unary operators on fixnum

--- spec/stdlib/fixnum_spec.rb
+++ spec/stdlib/fixnum_spec.rb
@@ -42,6 +42,25 @@ describe FastRuby, "fastruby fixnum stdlib" do
     
     $fixnum_class_num = $fixnum_class_num + 1
   end
+  
+  def self.test_unary_op(recv, op, expected = recv.send(op))
+    _classname = "FXNUMTEST#{$fixnum_class_num}"
+    
+    it "receiver #{recv}.#{op} should return #{expected} #{expected.class}" do
+    fastruby(
+      "class #{_classname}
+          def foo
+            #{recv}.#{op}
+          end
+        end
+      "
+    
+    )
+    
+      eval(_classname).new.foo.should be == expected
+    end
+    $fixnum_class_num = $fixnum_class_num + 1
+  end
 
   
   class CustomValue
@@ -60,6 +79,13 @@ describe FastRuby, "fastruby fixnum stdlib" do
       return self, other
     end
   end
+  
+  [:-@, :abs, :magnitude, :to_f, :size, :zero?, :odd?, :even?, :succ].each do |op|
+    test_unary_op(31,op)
+    test_unary_op(-31,op)
+  end
+  
+  #TODO: upto, downto, succ, next, pred, chr
 
   [:+, :-, :<, :>, :/,:div,:%,:modulo,:divmod,:fdiv,:**,:==,:===,:<=>,:>=,:<=].each do |op|
     [1,1.1,10**10,CustomValue.new].each do |value|
