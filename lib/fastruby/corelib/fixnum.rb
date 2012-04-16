33365727a0f7a952c63a34ffdd58e45557ba8760
Dario Seminara <darios3@gmail.com>
2012-04-15 22:32:36 -0300

    added upto, downto and times for fixnum

--- lib/fastruby/corelib/fixnum.rb
+++ lib/fastruby/corelib/fixnum.rb
@@ -22,6 +22,76 @@ require "fastruby/sexp_extension"
 
 class Fixnum
   fastruby(:fastruby_only => true, :skip_reduce => true) do
+    def times
+      unless block_given?
+        return _static{rb_enumeratorize(self, _dynamic{:times}, inline_c("0"), inline_c("0") ) }
+      end
+      
+      if self._class == Fixnum
+        i = 0
+        while _static{FIX2LONG(i) < FIX2LONG(self)}
+          yield(i)
+          i = _static{LONG2FIX(FIX2LONG(i)+1)}
+        end
+      else
+        i = 0
+        while i < self
+          yield(i)
+          i = i + 1
+        end
+      end
+    end
+    
+    def upto(x)
+      unless block_given?
+        return _static{rb_enumeratorize(self, _dynamic{:upto}, inline_c("1"), c_address_of(x)) }
+      end
+      if self._class == Fixnum
+        if x._class == Fixnum
+          i = self
+          while _static{FIX2LONG(i) <= FIX2LONG(x)}
+            yield(i)
+            i = _static{LONG2FIX(FIX2LONG(i)+1)}
+          end
+      
+          return self  
+        end
+      end
+      
+      i = self
+      while i <= x
+        yield(i)
+        i = i + 1
+      end
+      
+      self
+    end
+
+    def downto(x)
+      unless block_given?
+        return _static{rb_enumeratorize(self, _dynamic{:downto}, inline_c("1"), c_address_of(x)) }
+      end
+      
+      if self._class == Fixnum
+        if x._class == Fixnum
+          i = self
+          while _static{FIX2LONG(i) >= FIX2LONG(x)}
+            yield(i)
+            i = _static{LONG2FIX(FIX2LONG(i)-1)}
+          end
+      
+          return self  
+        end
+      end
+      
+      i = self
+      while i >= x
+        yield(i)
+        i = i - 1
+      end
+      
+      self
+    end
 
     if RUBY_VERSION =~ /^1\\.9/
       def +(b)
