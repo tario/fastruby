7b0af566cb231530b0f6bd2209ac13b1ec4fb6c8
Dario Seminara <darios3@gmail.com>
2012-08-21 10:19:26 -0300

    added benchmark8.rb, version of benchmark.rb without lvar_type directive

---
+++ benchmarks/benchmark8.rb
@@ -0,0 +1,48 @@
+require "rubygems"
+require "fastruby"
+
+class X
+  def foo(a,b)
+		return a+b
+	end
+end
+
+class Y
+	def bar(x)
+		i = 1000000
+
+    # removed lvar_type (see benchmark.rb)
+			
+		ret = 0
+		while i > 0
+			ret = x.foo(i,i)
+			i = i - 1
+		end
+		return ret
+	end
+end
+
+
+x = X.new
+y = Y.new
+
+require 'benchmark'
+
+Benchmark::bm(20) do |b|
+
+	b.report("ruby") do
+		y.bar(x)
+	end
+
+  X.optimize(:foo)
+  Y.optimize(:bar)
+  X.build([X,Fixnum,Fixnum],:foo)
+  Y.build([Y,X],:bar)
+  Fixnum.build([Fixnum,Fixnum],:-)
+  Fixnum.build([Fixnum,Fixnum],:>)
+  Fixnum.build([Fixnum,Fixnum],:+)
+
+	b.report("fastruby") do
+		y.bar(x)
+	end
+end
